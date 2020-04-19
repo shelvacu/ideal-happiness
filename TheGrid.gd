f7e1849182383e2ba4008e1f6bbc65e6ad8ebae9
Shelvacu <shelvacu@gmail.com>
2020-04-18 20:33:37 -0700

    blegh

--- TheGrid.gd
+++ TheGrid.gd
@@ -3,15 +3,17 @@ extends Node2D
 const Width = 10
 const Height = 10
 
-const InitTiles = [
-	[0, 1, "block"],
-	[1, 1, "block"],
-	[2, 1, "block"],
-	[3, 1, "bridge-fall"],
-	[4, 1, "block"],
-	[5, 1, "block"],
-	[1, 0, "button-depressed"],
-	[9, 9, "block"]
+onready var init_tiles = [
+	[0, 1, TileBlock.new()],
+	[1, 1, TileBlock.new()],
+	[2, 1, TileBlock.new()],
+	[3, 1, TileBridge.new()],
+	[4, 1, TileBlock.new()],
+	[5, 1, TileBlock.new()],
+	[0, 0, TileEnd.new()],
+	[1, 0, TileButton.new()],
+	[2, 0, TilePortal.new(-5)],
+	[9, 9, TileBlock.new()]
 ]
 
 const InitConnections = [
@@ -26,21 +28,22 @@ onready var tileScene = preload("res://Tile.tscn")
 
 enum Action { NONE, MOVE_UP, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT }
 
-func cartesian_product(of:Array) -> Array:
-	if of.size() == 0:
-		return []
-	elif of.size() == 1:
-		var res := []
-		for el in of[0]:
-			res.append([el])
-		return res
-	else:
-		var sub_problem := of.slice(1,-1)
-		var sub_res := cartesian_product(sub_problem)
-		var res := []
-		for el in of[0]:
-			res.append([el] + sub_res)
-		return res
+class Util:
+	static func cartesian_product(of:Array) -> Array:
+		if of.size() == 0:
+			return []
+		elif of.size() == 1:
+			var res := []
+			for el in of[0]:
+				res.append([el])
+			return res
+		else:
+			var sub_problem := of.slice(1,-1)
+			var sub_res := cartesian_product(sub_problem)
+			var res := []
+			for el in of[0]:
+				res.append([el] + sub_res)
+			return res
 
 class ProcessResult:
 	var isFail := false
@@ -50,7 +53,7 @@ class ProcessResult:
 class TileActor: #<T>
 	func _init():
 		pass
-	func process(x:int, y:int, isActive:bool, grid) -> ProcessResult:
+	func process(_x:int, _y:int, _isActive:bool, _grid) -> ProcessResult:
 		return ProcessResult.new()
 	func is_solid(_state) -> bool:
 		return false
@@ -61,8 +64,8 @@ class TileActor: #<T>
 		return [null]
 	func state(): #-> T
 		return null
-  func signal_out() -> bool:
-    return false
+	func signal_out() -> bool:
+		return false
 	func duplicate() -> TileActor:
 		return TileActor.new()
 
@@ -78,14 +81,14 @@ class TileBlock:
 class TileButton:
 	extends TileActor
 	var pressed := false
-	func process(x:int, y:int, isActive:bool, grid) -> ProcessResult:
+	func process(x:int, y:int, _isActive:bool, grid) -> ProcessResult:
 		if grid.playerHere(x, y):
 			pressed = true
 		var res := ProcessResult.new()
 		res.isTrigger = pressed
 		return res
-  func signal_out() -> bool:
-    return pressed
+	func signal_out() -> bool:
+		return pressed
 	func tile_name() -> String:
 		if pressed:
 			return "button-pressed"
@@ -99,11 +102,11 @@ class TileButton:
 class TileBridge:
 	extends TileActor
 	var fall := false
-	func process(x:int, y:int, isActive:bool, grid) -> ProcessResult:
+	func process(_x:int, _y:int, isActive:bool, _grid) -> ProcessResult:
 		fall = !isActive
 		return ProcessResult.new()
 	func is_solid(state:bool) -> bool:
-		return !fall
+		return !state
 	func tile_name() -> String:
 		if fall:
 			return "bridge-fall"
@@ -128,6 +131,13 @@ class TilePortal:
 	func duplicate() -> TileActor:
 		return TilePortal.new(steps)
 
+class TileEnd:
+	extends TileActor
+	func tile_name() -> String:
+		return "end"
+	func duplicate() -> TileActor:
+		return TileEnd.new()
+
 #class TilePlayer:
 #	extends TileActor
 #	var alive := true
@@ -151,7 +161,7 @@ class Player:
 	var lifetime:int = 0
 	var x:int
 	var y:int
-  var dead := false
+	var dead := false
 	func _init(x_:int, y_:int):
 		x = x_
 		y = y_
@@ -159,7 +169,7 @@ class Player:
 		var d = Player.new(x, y)
 		d.direction = direction
 		d.lifetime = lifetime
-    d.dead = dead
+		d.dead = dead
 		return d
 
 class Grid:
@@ -176,7 +186,7 @@ class Grid:
 			for _b in range(width):
 				row.append(null)
 			grid.append(row)
-	func at(x: int, y: int) -> Array:
+	func at(x: int, y: int): #-> TileActor|null
 		return grid[y][x]
 	func add(x: int, y: int, el: TileActor) -> void:
 		assert(grid[y][x] == null)
@@ -197,9 +207,13 @@ class Grid:
 #	var grid:Grid
 #	var next_nodes := []
 
+enum NodeState {Continue, Dead, End}
+
 class Timeline:
 	var root:Dictionary
-	func _init(root_grid:Grid, player_position:Array):
+	var start_position:Array
+	func _init(root_grid:Grid, start_position_:Array):
+		start_position = start_position_
 		root = {
 			time_step = 0,
 			player_step = 0,
@@ -207,7 +221,8 @@ class Timeline:
 			next_nodes = [],
 			constraints = [], #Array<(x,y,state)>
 			player_parent = null,
-			time_parent = null
+			time_parent = null,
+			state = NodeState.Continue
 		}
 		#var grid_one = root_grid.duplicate()
 		#var player = TilePlayer.new()
@@ -240,10 +255,9 @@ class Timeline:
 	func next_step(node:Dictionary) -> void: #result is in node.next_nodes
 		#assuming tile at x,y has state state, move player at playeridx by the vector vx,vy
 		var assumption_moves := [] #Array<Array<(Array<(x,y,state)>,playeridx,vx:int|null,vy:int|null)
-		for pi in range(new_grid.players.size()):
+		for pi in range(node.grid.players.size()):
 			assumption_moves.append([])
-			var p:Player = new_grid.players[pi]
-			var is_last:bool = pi == new_grid.players.size() - 1
+			var p:Player = node.grid.players[pi]
 			var vector_x := 0
 			var vector_y := 0
 			match p.direction:
@@ -307,8 +321,8 @@ class Timeline:
 									null, #they ded yo
 									null
 								])
-		for move_assumption_set in TheGrid.cartesian_product(assumption_moves):
-      var new_grid = node.grid.duplicate()
+		for move_assumption_set in Util.cartesian_product(assumption_moves):
+			var new_grid = node.grid.duplicate()
 			var constraints = []
 			for move_assumption in move_assumption_set:
 				for assumption in move_assumption[0]:
@@ -320,74 +334,94 @@ class Timeline:
 				next_nodes = [],
 				constraints = constraints,
 				player_parent = node,
-				time_parent = node
-        death = false
+				time_parent = node,
+				state = NodeState.Continue
 			}
-      for move_assumption in move_assumption_set:
-        var pi = move_assumption[1]
-        var vx = move_assumption[2]
-        var vy = move_assumption[3]
-        var p = new_grid.players[pi]
-        if vx == null:
-          new_node.death = true
-          p.dead = true
-        else:
-          p.x += vx
-          p.y += vy
-	    	for y in range(new_grid.height):
-	    		for x in range(new_grid.width):
-            var is_active := false
-            for conn in new_grid.connections:
-              if conn[2] == x and conn[3] == y and grid.get(conn[0],conn[1]).signal_out():
-                is_active = true
-            new_grid.get(x,y).process(x,y,is_active,new_grid)
+			for move_assumption in move_assumption_set:
+				var pi = move_assumption[1]
+				var vx = move_assumption[2]
+				var vy = move_assumption[3]
+				var p = new_grid.players[pi]
+				if vx == null:
+					new_node.state = NodeState.Dead
+					p.dead = true
+				else:
+					p.x += vx
+					p.y += vy
+			for y in range(new_grid.height):
+				for x in range(new_grid.width):
+					var is_active := false
+					for conn in new_grid.connections:
+						if conn[2] == x and conn[3] == y and node.grid.get(conn[0],conn[1]).signal_out():
+							is_active = true
+					new_grid.get(x,y).process(x,y,is_active,new_grid)
+			if node.time_step == 0:
+				new_grid.players.push_front(Player.new(start_position[0], start_position[1]))
+			if new_grid.players.size() > 0:
+				var last_player = new_grid.players[-1]
+				var block = new_grid.get(last_player.x, last_player.y)
+				if block != null and block is TilePortal:
+					assert(block.steps < 0)
+					var time_template = node
+					for _i in range(-block.steps):
+						time_template = time_template.time_parent
+					var new_new_grid = time_template.grid.duplicate()
+					new_new_grid.players.append(last_player.duplicate())
+					var time_parent = time_template.time_parent
+					new_node.time_step = time_template.time_step
+					new_node.grid = new_new_grid
+					new_node.time_parent = time_parent
+				elif block != null and block is TileEnd:
+					new_node.state = NodeState.End
+			node.children.append(new_node)
 		
-		var new_node = {
-			time_step = node.time_step + 1,
-			player_step = node.player_step + 1,
-			grid = new_grid,
-			next_nodes = [],
-			constraints = [],
-			player_parent = node,
-			time_parent = node
-		}
+		#var new_node = {
+		#	time_step = node.time_step + 1,
+		#	player_step = node.player_step + 1,
+		#	grid = new_grid,
+		#	next_nodes = [],
+		#	constraints = [],
+		#	player_parent = node,
+		#	time_parent = node
+		#}
 		
-		return []
-		pass
+		return 
+	func make_tree(node:Dictionary):
+		if node.state != NodeState.Continue: return
+		next_step(node)
+		for child_node in node.next_nodes:
+			make_tree(child_node)
 	func do_stuff():
 		pass
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for _a in range(Width):
-		var row := []
-		for _b in range(Height):
-			var cell := []
-			row.append(cell)
-		tiles.append(row)
-	for tilespec in InitTiles:
+	var root_grid = Grid.new(Width, Height)
+	for tilespec in init_tiles:
 		match tilespec:
 # warning-ignore:unassigned_variable
 # warning-ignore:unassigned_variable
 # warning-ignore:unassigned_variable
-			[var x, var y, var name]:
-				print(name)
-				tiles[y][x].append(name)
+			[var x, var y, var tile]:
+				root_grid.add(x, y, tile)
 			_:
 				assert(false)
-	tiles[InitPlayerPos[0]][InitPlayerPos[1]].append("noodly-alive")
-	var rowIdx := 0
-	for row in tiles:
-		var cellIdx := 0
-		for cell in row:
-			for tileName in cell:
-				var tileNode:Node2D = tileScene.instance()
-				tileNode.get_children()[0].animation = tileName
-				tileNode.position.x = cellIdx * 50
-				tileNode.position.y = rowIdx * 50
-				self.add_child(tileNode)
-			cellIdx += 1
-		rowIdx += 1
+	var timeline = Timeline.new(root_grid, InitPlayerPos)
+	timeline.make_tree(timeline.root)
+	print(timeline)
+	assert(false)
+	#var rowIdx := 0
+	#for row in tiles:
+	#	var cellIdx := 0
+	#	for cell in row:
+	#		for tileName in cell:
+	#			var tileNode:Node2D = tileScene.instance()
+	#			tileNode.get_children()[0].animation = tileName
+	#			tileNode.position.x = cellIdx * 50
+	#			tileNode.position.y = rowIdx * 50
+	#			self.add_child(tileNode)
+	#		cellIdx += 1
+	#	rowIdx += 1
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
