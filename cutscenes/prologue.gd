2f8f0511d60bc9b5defa2f531dc2072e089bb75a
Shelvacu <shelvacu@gmail.com>
2020-04-20 13:19:04 -0700

    instructions

--- cutscenes/prologue.gd
+++ cutscenes/prologue.gd
@@ -12,20 +12,21 @@ const Messages := [
 	"I mean look at all this stuff I'm saying",
 	"I can even say things\non multiple lines!"
 ]
+onready var textbox:RichTextLabel = self.find_node("DialogBoxText", true, false)
 
 var text_idx := -1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	textbox.text = "Click to see next text"
 	pass # Replace with function body.
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton and event.pressed:
 		if event.button_index == BUTTON_LEFT:
 			text_idx += 1
-			var text:RichTextLabel = self.find_node("DialogBoxText", true, false)
 			if text_idx < Messages.size():
-				text.text = Messages[text_idx]
+				textbox.text = Messages[text_idx]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
