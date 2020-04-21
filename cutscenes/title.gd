a4c1187a08517aac8f101c49f445fc47a3bace74
Shelvacu <shelvacu@gmail.com>
2020-04-20 17:07:26 -0700

    title now has button for level 1

---
+++ cutscenes/title.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == BUTTON_LEFT:
+			get_tree().change_scene("res://TheGrid.tscn")
